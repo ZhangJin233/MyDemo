c209cd0d70322a91764e5a98561d2b36ebd07b2a
ZhangJin233 <jane.zhangjin@outlook.com>
2020-01-12 15:19:21 +0800

    Add UIView
    视图UIView

--- MyDemo/ViewController.m
+++ MyDemo/ViewController.m
@@ -17,13 +17,18 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    [self.view addSubview:({
-        UILabel *label = [[UILabel alloc] init];
-        label.text = @"hello world";
-        [label sizeToFit];
-        label.center = CGPointMake(self.view.frame.size.width/2, self.view.frame.size.height/2);
-        label;
-    })];
+    UIView *view = [[UIView alloc] init];
+    view.backgroundColor = [UIColor redColor];
+    view.frame = CGRectMake(100, 100, 100, 100);
+    [self.view addSubview:view];
+    
+    UIView *view2 = [[UIView alloc] init];
+    view2.backgroundColor = [UIColor greenColor];
+    view2.frame = CGRectMake(150, 150, 100, 100);
+    [self.view addSubview:view2];
+    
+//    view 先入栈，view2 后入栈，view2显示在view上
+    
 }
 
 
