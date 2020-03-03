62e1d97cf0e07770c1e65e9f78063553e97bc042
ZhangJin233 <jane.zhangjin@outlook.com>
2020-03-03 22:59:16 +0800

    UIViewController

--- MyDemo/ViewController.m
+++ MyDemo/ViewController.m
@@ -45,6 +45,34 @@
 
 @implementation ViewController
 
+- (instancetype)init{
+    self = [super init];
+    if(self){
+        
+    }
+    return self;
+}
+
+- (void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+}   // Called when the view is about to made visible. Default does nothing
+
+
+- (void)viewDidAppear:(BOOL)animated{
+    [super viewDidAppear:animated];
+}    // Called when the view has been fully transitioned onto the screen. Default does nothing
+
+
+- (void)viewWillDisappear:(BOOL)animated{
+    [super viewWillDisappear:animated];
+} // Called when the view is dismissed, covered or otherwise hidden. Default does nothing
+
+
+- (void)viewDidDisappear:(BOOL)animated{
+    [super viewDidDisappear:animated];
+}  // Called after the view was dismissed, covered or otherwise hidden. Default does nothing
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
