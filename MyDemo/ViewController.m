6dffaa347cab400449c547fd76424dadb078836e
ZhangJin233 <jane.zhangjin@outlook.com>
2020-03-04 22:48:59 +0800

    UITableView

--- MyDemo/ViewController.m
+++ MyDemo/ViewController.m
@@ -77,24 +77,25 @@
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
     
-    
     // Do any additional setup after loading the view.
     //    UIView *view = [[UIView alloc] init];
     //    view.backgroundColor = [UIColor redColor];
     //    view.frame = CGRectMake(100, 100, 100, 100);
     //    [self.view addSubview:view];
     
-    TestView *view2 = [[TestView alloc] init];
-    view2.backgroundColor = [UIColor greenColor];
-    view2.frame = CGRectMake(150, 150, 100, 100);
-    [self.view addSubview:view2];
+    //    TestView *view2 = [[TestView alloc] init];
+    //    view2.backgroundColor = [UIColor greenColor];
+    //    view2.frame = CGRectMake(150, 150, 100, 100);
+    //    [self.view addSubview:view2];
+    //
+    //    //    view 先入栈，view2 后入栈，view2显示在view上
+    //
+    //
+    //    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(pushController)];
+    //    [view2 addGestureRecognizer:tapGesture];
     
-    //    view 先入栈，view2 后入栈，view2显示在view上
-    
-    
-    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(pushController)];
-    [view2 addGestureRecognizer:tapGesture];
-    
+    UITableView *tableView = [[UITableView alloc] initWithFrame:self.view.bounds];
+    [self.view addSubview:tableView];
 }
 
 - (void)pushController{
