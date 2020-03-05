f7b144d9114bb6f561357f5af77bfc916a9da24d
ZhangJin233 <jane.zhangjin@outlook.com>
2020-03-05 23:06:47 +0800

    UICollectionViewLayout  UICollectionViewFlowLayout

--- MyDemo/GTVideoViewController.m
+++ MyDemo/GTVideoViewController.m
@@ -30,6 +30,10 @@
     
     UICollectionViewFlowLayout *flowLayout = [[UICollectionViewFlowLayout alloc] init];
     
+    flowLayout.minimumLineSpacing = 10;
+    flowLayout.minimumInteritemSpacing = 10;
+    flowLayout.itemSize = CGSizeMake((self.view.frame.size.width - 10)/2,300);
+    
     UICollectionView *collectionView = [[UICollectionView alloc] initWithFrame:self.view.bounds collectionViewLayout:flowLayout];
     
     collectionView.delegate = self;
@@ -51,7 +55,7 @@
  */
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
-    return 200;
+    return 20;
 }
 
 // The cell that is returned must be retrieved from a call to -dequeueReusableCellWithReuseIdentifier:forIndexPath:
@@ -60,5 +64,14 @@
     cell.backgroundColor = [UIColor redColor];
     return cell;
 }
+
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath{
+    if(indexPath.item %3 ==0){
+        return CGSizeMake(self.view.frame.size.width,100);
+    }else{
+        return CGSizeMake((self.view.frame.size.width - 10)/2, 300);
+    }
+}
+
 
 @end
